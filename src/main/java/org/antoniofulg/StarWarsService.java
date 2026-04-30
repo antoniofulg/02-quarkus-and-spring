705bd35b11b1e5782de560df16eddbb46327d839
Antonio Fulgencio <antoniofulg@gmail.com>
2026-04-30 00:44:26 -0300

    Health check

--- src/main/java/org/antoniofulg/StarWarsService.java
+++ src/main/java/org/antoniofulg/StarWarsService.java
@@ -1,5 +1,8 @@
 package org.antoniofulg;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import jakarta.ws.rs.Consumes;
@@ -13,7 +16,16 @@ import jakarta.ws.rs.core.MediaType;
 @Consumes(MediaType.APPLICATION_JSON)
 public interface StarWarsService {
 
+  public static final String MSG_ERROR = "Fallback";
+
   @GET
   @Path("starships")
+  @Timeout(value = 3000L)
+  @Fallback(fallbackMethod = "getStarshipsFallback")
+  @CircuitBreaker(requestVolumeThreshold = 2, failureRatio = .5, delay = 3000L, successThreshold = 2)
   public String getStarships();
+
+  default public String getStarshipsFallback() {
+    return MSG_ERROR;
+  }
 }
